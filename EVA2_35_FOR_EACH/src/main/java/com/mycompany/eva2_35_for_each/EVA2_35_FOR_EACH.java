136a720061bf841f059363cc687e5c57f5b4523f
Venzorprog72 <143563081+Venzorprog72@users.noreply.github.com>
2023-11-22 17:50:26 -0600

    Add files via upload

---
+++ EVA2_35_FOR_EACH/src/main/java/com/mycompany/eva2_35_for_each/EVA2_35_FOR_EACH.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_35_for_each;
+
+/**
+ *
+ * @author Alfredo Venzor
+ */
+public class EVA2_34_FOR_EACH {
+
+    public static void main(String[] args) {
+        //FOR EACH --> FOR PARA RECORRER ARREGLOS
+        // SOLO SIRVE PARA LEER ARREGLOS
+        
+        int arreglo[] = new int[10];
+        for (int i = 0; i <arreglo.length; i++) {
+            arreglo [i] = (int)(Math.random() * 100);
+            {
+              
+            }
+            
+            
+             for (int valor : arreglo) {
+                 
+                         
+                
+            }
+        }
+    }
+}
